3841e4e7a9a9143e819d3aed8c7df0e1acf5d4b1
Inframatic <isteffy@hotmail.com>
2013-07-21 22:30:05 -0400

    refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Photogurpractice::Application.routes.draw do
-
-  get 'pictures' => 'pictures#index'
-
-  get 'pictures/:id' => 'pictures#show', as: "picture"
-
-  post 'pictures' => "pictures#create"
-  get 'pictures/new' => 'pictures#new' 
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-put 'pictures/:id' => "pictures#update", as: "update_picture"
-end
+  resources :pictures
+  root :to => "pictures#index"
+  end
