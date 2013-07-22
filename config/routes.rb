041fcbf9a2abbbad1b15db75d0879ea9e08887e5
Inframatic <isteffy@hotmail.com>
2013-07-21 22:13:32 -0400

    Refactoring Step #6 to use Formhelpers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Photogurpractice::Application.routes.draw do
   get 'pictures' => 'pictures#index'
 
   get 'pictures/:id' => 'pictures#show', as: "picture"
+
+  post 'pictures' => "pictures#create"
+  get 'pictures/new' => 'pictures#new' 
 end
