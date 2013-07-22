ed751d5c2b847651edc402e6ba70cd22fa56a7ee
Inframatic <isteffy@hotmail.com>
2013-07-21 21:21:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogurpractice::Application.config.secret_key_base = '55e88898669ae55c6ea678525faeb029dcde9355125cd268e2f045b48b4f183870660b0d1cb66f9dcad93cf49bd14642a8db823671d9f9604607ef0fa9c7c4f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Photogurpractice::Application.config.session_store :cookie_store, key: '_photogurpractice_session'
